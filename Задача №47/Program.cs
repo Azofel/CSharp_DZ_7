05ac73cac1ad66acabe9522629ad3bfcd97ed248
Azofel <136746291+Azofel@users.noreply.github.com>
2023-07-24 20:35:45 +0300

    CSharp_DZ_7
    CSharp_DZ_7

---
+++ Задача №47/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите размерность m массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] randomArray = new double[m,n];
+
+void array(int m, int n)
+{
+    int i, j;
+    Random chaotic = new Random();
+    for (i = 0; i < m; i++)
+    {
+        Console.WriteLine();
+        for (j = 0; j < n; j++)
+            {
+                randomArray[i,j] = chaotic.NextDouble();
+                Console.Write($"{randomArray[i,j]:f} ");
+            }
+    }
+}
+
+array(m,n);
